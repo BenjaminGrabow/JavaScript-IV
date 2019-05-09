db56971a774d2b91f192cbf51efd7c527166ba13
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-05-09 16:02:29 +0200

    MVP complete

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,6 +1,6 @@
 // PERSON
 class Person {
-    constructer(personAttributes) {
+    constructor(personAttributes) {
         this.name = personAttributes.name;
         this.age = personAttributes.age;
         this.location = personAttributes.location;
@@ -8,7 +8,7 @@ class Person {
     }
 
     speak() {
-        return `Hello my name is ${this.name}, I am from ${this.location}`
+        return `Hello my name is ${this.name}, I am from ${this.location}.`
     }
 }
 
@@ -18,20 +18,21 @@ const thomas = new Person({
     location: 'Berlin',
     gender: 'male'
 });
+console.log(thomas.speak());
 // INSTRUCTOR
 
 class Instructor extends Person {
-    constructer(instructorAttributes) {
+    constructor(instructorAttributes) {
         super(instructorAttributes);
         this.specialty = instructorAttributes.specialty;
         this.favLanguage = instructorAttributes.favLanguage;
         this.catchPhrase = instructorAttributes.catchPhrase;
     }
     demo(subject) {
-        return 'Today we are learning about {subject}';
+        return `Today we are learning about ${subject}`;
     }
     grade(student, subject) {
-        return '{student.name} receives a perfect score on {subject}';
+        return `{student.name} receives a perfect score on ${subject}`;
     }
 }
 
@@ -53,10 +54,11 @@ const berta = new Instructor({
     favLanguage: 'Python',
     catchPhrase: 'When you feel like quitting, think about why you started.'
 });
+console.log(berta.demo("Javascript"));
 // STUDENT
 
 class Student extends Person {
-    constructer(studentAttributes) {
+    constructor(studentAttributes) {
         super(studentAttributes);
         this.previousBackground = studentAttributes.previousBackground;
         this.className = studentAttributes.className;
@@ -66,10 +68,10 @@ class Student extends Person {
         return this.favSubjects;
     }
     PRAssignment(subject) {
-        return `${this.name} has submitted a PR for {subject}`;
+        return `${this.name} has submitted a PR for ${subject}`;
     }
     sprintChallenge(subject) {
-        return `student.name has begun sprint challenge on {subject}`;
+        return `${this.name} has begun sprint challenge on ${subject}`;
     }
 }
 
@@ -103,25 +105,28 @@ const gustav = new Student({
     location: 'München',
     gender: 'Male',
     specialty: 'none',
-    favLanguage: 'English',
+    favLanguage: 'Python',
     catchPhrase: 'The only way to do great work is to love what you do.',
     previousBackground: 'cable worker',
     className: 'WebEu2',
     favSubjects: ['CSS ', 'React ']
 })
+console.log(gustav.listsSubjects());
+console.log(bernd.PRAssignment("CSS"));
+console.log(gregor.sprintChallenge("Javascript"))
 // PROJECTMANAGER
 
 class ProjectManager extends Instructor {
-    constructer(projectManagerAttributes) {
+    constructor(projectManagerAttributes) {
         super(projectManagerAttributes);
         this.gradClassName = projectManagerAttributes.gradClassName;
         this.favInstructor = projectManagerAttributes.favInstructor;
     }
     standUp(name, channel) {
-        return `{name} announces to {channel}, @channel standy times!​​​​​`;
+        return `${name} announces to ${channel}, @channel standy times!​​​​​`;
     }
     debugsCode(student, subject) {
-        return `{ name } debugs { student.name } 's code on {subject}`;
+        return `${this.name} debugs ${student}'s code on ${subject}`;
     }
 }
 const claudia = new ProjectManager({
@@ -146,12 +151,6 @@ const sabine = new ProjectManager({
     gradClassName: 'WebEu1',
     favInstructor: 'Berta'
 });
-
-#### Stretch Problem
-
-    * Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1 - 100.
-        * Now that our students have a grade build out a method on the Instructor(this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade._Math.random_ will help.
-* Add a graduate method to a student.
-  * This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
-    * If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
+console.log(sabine.standUp("Bernd", "WebEu2"));
+console.log(claudia.debugsCode("Gregor", "VSC"));
 
